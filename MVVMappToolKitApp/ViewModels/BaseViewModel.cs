27916e702e095db8046fa7b2c47670364af9593f
Luiz Carlos de O. Nolasco <13737927+lconolasco@users.noreply.github.com>
2022-10-13 10:14:12 +0200

    UI - Foi substituid o ListView pelo CollectionView para contornar um erro de casting de dados.
    UX - A CollectionView agora é Scrolling, isto é, pode-se navegar por todo o conteùdo desta.
    
    Back End - Foi incluido uma BaseViewModel para suportar novas implentaçoes atuais e futuras. Foram efetuados refattoramento do codigo corrigindo alguns bugs.
    
    Bug fix - Label de messagem sò exibia os erros apenas com o App apena carregado, nao mostrando os eventos de erros apòs o inserimento de um dado.
    
    A Lista de nomes nao era navegavel. Esta limitava-se a exibir uns 10 ou 12 registros.

---
+++ MVVMappToolKitApp/ViewModels/BaseViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MVVMappToolKitApp.ViewModels
+{
+    public partial class BaseViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+        private bool isBusy;
+
+        [ObservableProperty]
+        string title;
+
+        public bool IsNotBusy => !IsBusy;
+
+        public BaseViewModel()
+        {
+
+        }
+    }
+}
